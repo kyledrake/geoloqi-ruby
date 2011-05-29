847e99e7080a6f5cf3d39fe99b6401ccb1b556f9
Kyle Drake <kyledrake@gmail.com>
2011-05-29 15:28:10 -0700

    Huge refactor to enable highly flexible config, more oo. Multiple Geoloqi apps could be run from one app now

--- geoloqi.gemspec
+++ geoloqi.gemspec
@@ -1,11 +1,12 @@
 Gem::Specification.new do |s|
-  s.name = 'geoloqi-ruby'
+  s.name = 'geoloqi'
   s.version = '0.0.1'
   s.authors = ['Kyle Drake', 'Aaron Parecki']
   s.email = ['kyledrake@gmail.com', 'aaron@parecki.com']
-  s.homepage = 'https://github.com/kyledrake/reactor'
-  s.summary = 'Simple, flexible, lightweight interface to the awesome Geoloqi platform API'
-  s.description = 'Simple, flexible, lightweight interface to the awesome Geoloqi platform API!'
+  s.homepage = 'https://github.com/kyledrake/geoloqi-ruby'
+  s.summary = 'Powerful, flexible, lightweight interface to the awesome Geoloqi platform API'
+  s.description = 'Powerful, flexible, lightweight interface to the awesome Geoloqi platform API! Uses Faraday, '+
+                  'and can be used with Ruby 1.9 and EM-Synchrony for really fast, highly concurrent development.'
 
   s.files = `git ls-files`.split("\n")
   s.require_paths = %w[lib]
@@ -17,7 +18,6 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency 'wrong', '= 0.5.0'
   s.add_development_dependency 'minitest'
-  s.add_development_dependency 'rake'
   s.add_development_dependency 'em-http-request'
   s.add_development_dependency 'em-synchrony'
 end
--- spec/geoloqi_spec.rb
+++ spec/geoloqi_spec.rb
@@ -1,40 +1,79 @@
-raise ArgumentError, 'OAuth token is required. Get it from your Geoloqi.com account settings page' unless ARGV[0]
-
+raise ArgumentError, 'usage: be ruby spec/geoloqi_spec.rb "client_id" "client_secret" "oauth_token"' unless ARGV.length == 3
 Bundler.setup
-
-require 'geoloqi-ruby'
+require 'geoloqi'
 require 'minitest/autorun'
 require 'wrong'
 require 'wrong/adapters/minitest'
-require 'eventmachine'
-require 'em-http-request'
-require 'em-synchrony'
 
 Wrong.config.alias_assert :expect
 
 describe Geoloqi do
+  it 'reads geoloqi config' do
+    Geoloqi.config :client_id => 'client_id', :client_secret => 'client_secret'
+    expect { Geoloqi.config.is_a?(Geoloqi::Config) }
+  end
+  
+  it 'returns authorize url' do
+    authorize_url = Geoloqi.authorize_url 'test', 'http://blah.blah/test'
+    expect { authorize_url == "#{Geoloqi::API_URL}/oauth/authorize?"+
+                              'response_type=code&'+
+                              "client_id=#{Rack::Utils.escape 'test'}&"+
+                              "redirect_uri=#{Rack::Utils.escape 'http://blah.blah/test'}" }
+  end
+end
+
+describe Geoloqi::Config do
   it 'throws exception if non-boolean value is fed to logging' do
-    expect { rescuing { Geoloqi.enable_logging :cats }.class == ArgumentError }
+    expect { rescuing { Geoloqi.config(:client_id => '', :client_secret => '', :enable_logging => :cats )}.class == ArgumentError }
   end
+end
 
-  it 'successfully makes call to api with forward slash' do
-    @response = Geoloqi.get ARGV[0], '/layer/info/Gx'
-    expect { @response['layer_id'] == 'Gx' }
-  end
-
-  it 'successfully makes call to api with net/http' do
-    Geoloqi.adapter :net_http
-    @response = Geoloqi.get ARGV[0], 'layer/info/Gx'
-    expect { @response['layer_id'] == 'Gx' }
-  end
-
-  it 'successfully makes call to api with em-synchrony' do
-    EM.synchrony do
-      # Geoloqi.enable_logging true
-      Geoloqi.adapter :em_synchrony
-      @response = Geoloqi.get ARGV[0], 'layer/info/Gx'
-      EM.stop
+describe Geoloqi::Session do
+  describe 'with no config' do
+    before do
+      @session = Geoloqi::Session.new :oauth_token => ARGV[2]
     end
-    expect { @response['layer_id'] == 'Gx' }
+    
+    it 'successfully makes call to api with forward slash' do
+      response = @session.get '/layer/info/Gx'
+      expect { response['layer_id'] == 'Gx' }
+    end
+
+    it 'successfully makes call to api without forward slash' do
+      response = @session.get '/layer/info/Gx'
+      expect { response['layer_id'] == 'Gx' }
+    end
   end
+
+  describe 'with em synchrony adapter and oauth_token' do
+    it 'makes call to api' do
+      session = Geoloqi::Session.new :oauth_token => ARGV[2], :config => Geoloqi::Config.new(:adapter => :em_synchrony)
+      response = session.get 'layer/info/Gx'
+      expect { response['layer_id'] == 'Gx' }
+    end
+  end
+  
+  describe 'with oauth id, secret, and oauth token via Geoloqi::Config' do
+    it 'should load config' do
+      @session = Geoloqi::Session.new :oauth_token => ARGV[2], :config => Geoloqi::Config.new(:client_id => ARGV[0], 
+                                                                                              :client_secret => ARGV[1])
+      expect { @session.config.client_id == ARGV[0] }
+      expect { @session.config.client_secret == ARGV[1] }
+    end
+  end
+  
+  describe 'with oauth id, secret, and oauth token via direct hash' do
+    before do
+      @session = Geoloqi::Session.new :oauth_token => ARGV[2], :config => {:client_id => ARGV[0], :client_secret => ARGV[1]}
+    end
+    
+    it 'gets authorize url' do
+      authorize_url = @session.authorize_url('http://blah.blah/test')
+      expect { authorize_url == "#{Geoloqi::API_URL}/oauth/authorize?"+
+                                "response_type=code&"+
+                                "client_id=#{Rack::Utils.escape ARGV[0]}&"+
+                                "redirect_uri=#{Rack::Utils.escape 'http://blah.blah/test'}" }
+    end
+  end
+  
 end
